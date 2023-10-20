951fc6044c1db75f1af07b514d8ba5c8725e1ce0
kere199 <kereleishviliirakli81@gmail.com>
2023-10-20 07:11:45 +0400

    move_right function and basic logic of the game

--- board.py
+++ board.py
@@ -1,17 +1,38 @@
 from turtle import Turtle,Screen
+import random
 class Board:
     def __init__(self):
      self.t = Turtle()
      self.t.hideturtle()
      self.t.penup()
-     self.t.goto(-100,300)
      self.t.setheading(0)
-     self.tiles = [[0,0,0,0], [0,0,0,0], [0,0,0,0], [0,0,0,0]]
+     self.tiles = [[0,0,0,0], [0,2,2,2], [0,0,2,0], [0,0,0,0]]
 
 
     def draw_tiles(self):
+       self.t.clear()
+       self.t.goto(-100,300)
        for row  in self.tiles:
           for num in row:
              self.t.write(num)
              self.t.forward(50) 
           self.t.goto(-100, self.t.ycor() - 50)
+
+
+    def move_right(self):
+        for row in range(4):
+            for col in range(3, 0, -1):
+                if self.tiles[row][col] != 0:
+                    x = col
+                    while x < 3 and self.tiles[row][x + 1] == 0:
+                        self.tiles[row][x + 1] = self.tiles[row][x]
+                        self.tiles[row][x] = 0
+                        x += 1
+                    if x < 3 and self.tiles[row][x + 1] == self.tiles[row][x]:
+                        self.tiles[row][x + 1] += self.tiles[row][x]
+                        self.tiles[row][x] = 0
+        self.draw_tiles()
+
+                   
+       
+      
--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 from turtle import Turtle,Screen
 from board import Board
-
+screen = Screen()
+screen.tracer(0)
 board = Board()
 board.draw_tiles()
+
+
+screen.listen()
+screen.onkeypress(fun=board.move_right, key = "Right")
+
+screen.update()
+
+
+screen.exitonclick()
